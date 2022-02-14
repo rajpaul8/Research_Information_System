28198da79db9cf03e6af893f9d6e18b1119ad942
rajpaul8 <rajpaul83@gmail.com>
2022-02-15 01:38:21 +0530

    Filtering Logic Completed

--- RISDatabase/views.py
+++ RISDatabase/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import *
-from .Drill_Down_Filter_Forms import *
 from django_pivot.pivot import pivot
 import pandas as pd
 from datetime import date
@@ -10,8 +9,8 @@ from django.db.models import Sum, Count
 def RISDB(request):
     RIS_Project_Objects = RIS_Project.objects.all()
     #####################################################################################
-    #                                  For Filters-                                   #
-    #######################################################################################
+    #                                  For Dependent Filters ---                        #
+    #####################################################################################
     # Partner Country is dependant on Sub Region and sub region is dependant on Partner Region
     Partner_Region_Choices = Partner_Region.objects.all()
     Sub_Region_Choices = Sub_Region.objects.all()
@@ -30,6 +29,57 @@ def RISDB(request):
     Year_Choices = RIS_Project.objects.values('Year').distinct()
 
     #####################################################################################
+    #                                  Filters Logic---                                 #
+    #####################################################################################
+
+    if 'PartnerRegion' in request.GET:
+        PartnerRegion = request.GET.getlist('PartnerRegion')
+        # print(PartnerRegion)
+        if PartnerRegion:
+            RIS_Project_Objects = RIS_Project_Objects.filter(Partner_Region_Code__in=PartnerRegion)
+
+    if 'SubRegion' in request.GET:
+        SubRegion = request.GET.getlist('SubRegion')
+        # print(SubRegion)
+        if SubRegion:
+            RIS_Project_Objects = RIS_Project_Objects.filter(Code_of_Sub_Region__in=SubRegion)
+
+    if 'PartnerCountry' in request.GET:
+        PartnerCountry = request.GET.getlist('PartnerCountry')
+        # print(PartnerCountry)
+        if PartnerCountry:
+            RIS_Project_Objects = RIS_Project_Objects.filter(Code_of_Sub_Region__in=PartnerCountry)
+
+    if 'Modalities' in request.GET:
+        Modalities = request.GET.getlist('Modalities')
+        # print(Modalities)
+        if Modalities:
+            RIS_Project_Objects = RIS_Project_Objects.filter(Modalities__in=Modalities)
+
+    if 'SubModalities' in request.GET:
+        SubModalities = request.GET.getlist('SubModalities')
+        # print(SubModalities)
+        if SubModalities:
+            RIS_Project_Objects = RIS_Project_Objects.filter(Sub_Modalities__in=SubModalities)
+
+
+    if 'Sector' in request.GET:
+        Sector = request.GET.getlist('Sector')
+        # print(Sector)
+        if Sector:
+            RIS_Project_Objects = RIS_Project_Objects.filter(Sector__in=Sector)
+
+    if 'YearFrom' in request.GET:
+        YearFrom = request.GET['YearFrom']
+        if YearFrom:
+            RIS_Project_Objects = RIS_Project_Objects.filter(Year__gte=YearFrom)
+
+    if 'YearTo' in request.GET:
+        YearTo = request.GET['YearTo']
+        if YearTo:
+            RIS_Project_Objects = RIS_Project_Objects.filter(Year__lte=YearTo)
+
+        #####################################################################################
     #                                  For Left Sidebar Graphs -                         #
     #######################################################################################
     # 1 Display Region-Wise Number Of Projects
@@ -73,7 +123,7 @@ def RISDB(request):
 
     context = {
         'RIS_Project_Objects': RIS_Project_Objects,
-        # Filtering Form
+        # --------------- Filtering Form ---------------------- #
         'Partner_Region_Choices': Partner_Region_Choices,
         'Sub_Region_Choices': Sub_Region_Choices,
         'Partner_Country_Choices': Partner_Country_Choices,
@@ -100,3 +150,7 @@ def Load_Dependent_Partner_Country_Filters(request):
     Sub_Region_id = request.GET.getlist('SubRegionId[]')
     partnerCountry = Partner_Country.objects.filter(Sub_Region_Name_id__in=Sub_Region_id).distinct()
     return render(request, 'partials/drill_down_filters/DrilDown_Partner_Country_Filter.html', {'Partner_Country': partnerCountry})
+
+
+
+
